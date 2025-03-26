7120b61ea957a5ed25c804bbeef8651ae27f418a
markoldmark <marcoldmark@gmail.com>
2025-03-26 18:12:51 +0100

    1.3.2
    Tabelle fatte a mano + prova draft

---
+++ script_draft.js
@@ -0,0 +1,31 @@
+function sx_draft(){
+    let scell00 = document.getElementById("s00");
+    let s00 = document.createElement("img");
+    s00.className = "santino";
+    s00.src = "https://img.a.transfermarkt.technology/portrait/header/28003-1740766555.jpg?lm=1";
+    s00.alt = "Messi";
+    scell00.appendChild(s00);
+
+    let scell01 = document.getElementById("s01");
+    let s01 = document.createElement("img");
+    s01.className = "santino";
+    s01.src = "https://img.a.transfermarkt.technology/portrait/header/8198-1694609670.jpg?lm=1";
+    s01.alt = "Ronaldo";
+    scell01.appendChild(s01);
+
+    let scell02 = document.getElementById("s02");
+    let s02 = document.createElement("img");
+    s02.className = "santino";
+    s02.src = "https://img.a.transfermarkt.technology/portrait/header/68290-1692601435.jpg?lm=1";
+    s02.alt = "Neymar";
+    scell02.appendChild(s02);
+
+    let scell03 = document.getElementById("s03");
+    let s03 = document.createElement("img");
+    s03.className = "santino";
+    s03.src = "https://img.a.transfermarkt.technology/portrait/header/43722-1718986946.jpg?lm=1";
+    s03.alt = "Isla";
+    scell03.appendChild(s03);
+}
+
+sx_draft();
