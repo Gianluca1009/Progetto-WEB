329fc6c36ac0161de435b2e7090f6f32e46390c1
Gianluca2003 <gianlu.nanni@gmail.com>
2025-03-31 23:14:39 +0200

    oriolo power

--- script_pedine.js
+++ script_pedine.js
@@ -142,35 +142,6 @@ function posizion_iniziali(){
         cell4.appendChild(pedone_bianco);
     }
 
-    // document.querySelectorAll(".pedina").forEach(ped =>{
-    //     ped.addEventListener("dragstart", function(event) {
-    //         // Salva l'elemento che stai trascinando
-    //         draggedElement = event.target;
-    //         event.dataTransfer.setData("text", event.target.id);
-    //     });
-    // });
-
-    // document.querySelectorAll("td").forEach(cell => {
-    //     // Permetti il drop sulle celle della tabella
-    //         cell.addEventListener("dragover", function(event) {
-    //         event.preventDefault();  // Necessario per consentire il drop
-    //     });
-
-    //     // Gestisci il drop sulle celle della tabella
-    //     cell.addEventListener("drop", function(event) {
-    //         event.preventDefault();
-
-    //         // Ottieni la cella di destinazione
-    //         let x = event.clientX;
-    //         let y = event.clientY;
-    //         let cell = document.elementFromPoint(x, y);
-
-    //         // Se è una cella (tagName == "TD"), aggiungi l'elemento
-    //         if (cell && cell.tagName === "TD") {
-    //             cell.appendChild(draggedElement);
-    //         }
-    //     });
-    // });
 }
 
 window.onload = posizion_iniziali();
