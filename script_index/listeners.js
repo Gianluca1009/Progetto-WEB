6818ba928e0151c17c95f333dea959d3279ecf5f
markoldmark <marcoldmark@gmail.com>
2025-04-13 14:02:53 +0200

    5.1.2.2
    comando gioca

---
+++ script_index/listeners.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const comandoGioca = document.getElementById('comandoGioca');
+    comandoGioca.addEventListener('click', function() {
+        window.location.href = 'gamepage.html';
+    });
+});
+
