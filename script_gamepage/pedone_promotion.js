56e1bd7881e8cd3756960a8f75eddfb874718370
markoldmark <marcoldmark@gmail.com>
2025-04-14 15:05:17 +0200

    5.5.7

--- script_gamepage/pedone_promotion.js
+++ script_gamepage/pedone_promotion.js
@@ -6,33 +6,39 @@ function upgrade_pedone(img_pedina, cella_dest){
     if(div_pedina.id == "p" ){
         if(cur_row == 0){
             document.getElementById("div_ped_promotion").classList.remove('hidden');
-<<<<<<< Updated upstream
             list_pedPromotion(div_pedina, img_pedina, window.turnoBianco);
-=======
-            list_pedPromotion(div_pedina, img_pedina);
->>>>>>> Stashed changes
         }
     }
     //nero arriva al fondo scacchiera
     if(div_pedina.id == "P" ){
         if(cur_row == 5){
             document.getElementById("div_ped_promotion").classList.remove('hidden');
-<<<<<<< Updated upstream
             list_pedPromotion(div_pedina, img_pedina, window.turnoBianco);
         } 
     }
 }
 
 function list_pedPromotion(pedina_d, img_pedina, turnoBianco){
-
+    // Rimuovo event listener esistenti prima di aggiungerne di nuovi
+    const reginaElement = document.getElementById('pedonepromotion_regina');
+    const cavalloElement = document.getElementById('pedonepromotion_cavallo');
+    
+    // Rimuovo vecchi listener usando cloni della tabella perche sui vecchi rimane il listener precedente
+    const reginaClone = reginaElement.cloneNode(true);
+    const cavalloClone = cavalloElement.cloneNode(true);
+    
+    reginaElement.parentNode.replaceChild(reginaClone, reginaElement);      
+    cavalloElement.parentNode.replaceChild(cavalloClone, cavalloElement);
+    
+    // Aggiungo nuovi listener che agiscono solo sul pedone attuale
     document.getElementById('pedonepromotion_regina').addEventListener('click', () =>{
         tipo = 'regina';
         //modifica il div con un nuovo pezzo
         if(turnoBianco){
-            pedina_d.id = pezzi[tipo].id.bianco ;
+            pedina_d.id = pezzi[tipo].id.bianco;
             img_pedina.src = pezzi[tipo].img.bianco;
         }else{
-            pedina_d.id = pezzi[tipo].id.nero ;
+            pedina_d.id = pezzi[tipo].id.nero;
             img_pedina.src = pezzi[tipo].img.nero;
         }
         document.getElementById("div_ped_promotion").classList.add('hidden');
@@ -42,51 +48,13 @@ function list_pedPromotion(pedina_d, img_pedina, turnoBianco){
         tipo = 'cavallo';
         //modifica il div con un nuovo pezzo
         if(turnoBianco){
-            pedina_d.id = pezzi[tipo].id.bianco ;
+            pedina_d.id = pezzi[tipo].id.bianco;
             img_pedina.src = pezzi[tipo].img.bianco;
         }else{
-            pedina_d.id = pezzi[tipo].id.nero ;
-=======
-            list_pedPromotion(div_pedina, img_pedina);
-        } 
-    }
-}
-function func_list_regina (div_pedina, img_pedina){
-        tipo = 'regina';
-        //modifica il div con un nuovo pezzo
-        if(window.turnoBianco){
-            div_pedina.id = pezzi[tipo].id.bianco ;
-            img_pedina.src = pezzi[tipo].img.bianco;
-        }else{
-            div_pedina.id = pezzi[tipo].id.nero ;
-            img_pedina.src = pezzi[tipo].img.nero;
-        }
-        document.getElementById("div_ped_promotion").classList.add('hidden');
-}
-function list_pedPromotion(div_pedina, img_pedina){
-
-    //document.getElementById('pedonepromotion_regina').addEventListener('click', func_list_regina(div_pedina,img_pedina));
-
-    document.getElementById('pedonepromotion_cavallo').addEventListener('click', () =>{
-        let div_pedina = window.selectedElement;
-        let img_pedina = window.selectedImage;
-        console.log(div_pedina);
-        console.log(img_pedina)
-        tipo = 'cavallo';
-        //modifica il div con un nuovo pezzo
-        if(window.turnoBianco){
-            div_pedina.id = pezzi[tipo].id.bianco ;
-            img_pedina.src = pezzi[tipo].img.bianco;
-        }else{
-            div_pedina.id = pezzi[tipo].id.nero ;
->>>>>>> Stashed changes
+            pedina_d.id = pezzi[tipo].id.nero;
             img_pedina.src = pezzi[tipo].img.nero;
         }
         document.getElementById("div_ped_promotion").classList.add('hidden');
     });
-<<<<<<< Updated upstream
 }
 
-=======
-}
->>>>>>> Stashed changes
