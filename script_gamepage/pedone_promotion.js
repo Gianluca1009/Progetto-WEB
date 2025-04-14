38c2e876dda51557b36f5c88125a07b31021530e
silver5001 <malara.2067824@studenti.uniroma1.it>
2025-04-14 14:50:03 +0200

    bozza con bug pedone promotion

--- script_gamepage/pedone_promotion.js
+++ script_gamepage/pedone_promotion.js
@@ -6,13 +6,18 @@ function upgrade_pedone(img_pedina, cella_dest){
     if(div_pedina.id == "p" ){
         if(cur_row == 0){
             document.getElementById("div_ped_promotion").classList.remove('hidden');
+<<<<<<< Updated upstream
             list_pedPromotion(div_pedina, img_pedina, window.turnoBianco);
+=======
+            list_pedPromotion(div_pedina, img_pedina);
+>>>>>>> Stashed changes
         }
     }
     //nero arriva al fondo scacchiera
     if(div_pedina.id == "P" ){
         if(cur_row == 5){
             document.getElementById("div_ped_promotion").classList.remove('hidden');
+<<<<<<< Updated upstream
             list_pedPromotion(div_pedina, img_pedina, window.turnoBianco);
         } 
     }
@@ -41,9 +46,47 @@ function list_pedPromotion(pedina_d, img_pedina, turnoBianco){
             img_pedina.src = pezzi[tipo].img.bianco;
         }else{
             pedina_d.id = pezzi[tipo].id.nero ;
+=======
+            list_pedPromotion(div_pedina, img_pedina);
+        } 
+    }
+}
+function func_list_regina (div_pedina, img_pedina){
+        tipo = 'regina';
+        //modifica il div con un nuovo pezzo
+        if(window.turnoBianco){
+            div_pedina.id = pezzi[tipo].id.bianco ;
+            img_pedina.src = pezzi[tipo].img.bianco;
+        }else{
+            div_pedina.id = pezzi[tipo].id.nero ;
+            img_pedina.src = pezzi[tipo].img.nero;
+        }
+        document.getElementById("div_ped_promotion").classList.add('hidden');
+}
+function list_pedPromotion(div_pedina, img_pedina){
+
+    //document.getElementById('pedonepromotion_regina').addEventListener('click', func_list_regina(div_pedina,img_pedina));
+
+    document.getElementById('pedonepromotion_cavallo').addEventListener('click', () =>{
+        let div_pedina = window.selectedElement;
+        let img_pedina = window.selectedImage;
+        console.log(div_pedina);
+        console.log(img_pedina)
+        tipo = 'cavallo';
+        //modifica il div con un nuovo pezzo
+        if(window.turnoBianco){
+            div_pedina.id = pezzi[tipo].id.bianco ;
+            img_pedina.src = pezzi[tipo].img.bianco;
+        }else{
+            div_pedina.id = pezzi[tipo].id.nero ;
+>>>>>>> Stashed changes
             img_pedina.src = pezzi[tipo].img.nero;
         }
         document.getElementById("div_ped_promotion").classList.add('hidden');
     });
+<<<<<<< Updated upstream
 }
 
+=======
+}
+>>>>>>> Stashed changes
