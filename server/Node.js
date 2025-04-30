01ef9247dd9defce4a51abf9a71bf73729d90f5d
markoldmark <marcoldmark@gmail.com>
2025-04-30 11:32:03 +0200

    6.0.1

---
+++ server/Node.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'user',
+    password: 'userpwd',
+    database: 'ChessDB'
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to MySQL database');
+});
