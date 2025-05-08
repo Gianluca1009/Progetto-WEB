8ac057e080bfd18ef1336933bff7dffec6c4dce4
markoldmark <marcoldmark@gmail.com>
2025-05-08 18:06:34 +0200

    8.2.3

--- publicHTML/script_gamepage/Regole.js
+++ publicHTML/script_gamepage/Regole.js
@@ -23,11 +23,13 @@ updateText(toggle.checked);
 
 // aggiorna il testo ogni volta che cambia
 toggle.addEventListener('change', () => {
-  stile = updateText(toggle.checked);
-  changeStyle(stile);
+    stile = updateText(toggle.checked);
+    changeStyle(stile);
 });
 
 function updateText(checked) {
-  text.textContent = checked ? 'MODERN' : 'CLASSIC';
-  return text.textContent;
+    text.textContent = checked ? 'MODERN' : 'CLASSIC';
+    if(text.textContent === 'MODERN') text.style.color = 'white';
+    else text.style.color = 'black';
+    return text.textContent;
 }
