75c49d3b288a082f23aaef3b1a6957d67272c875
markoldmark <marcoldmark@gmail.com>
2025-05-10 12:26:15 +0200

    9.1.3
    mercato aggiornato

--- publicHTML/script_mercato/Utility.js
+++ publicHTML/script_mercato/Utility.js
@@ -24,41 +24,78 @@ function convertDate(stringa){
 
 
 function BuildRowForCalciatore(calciatore){
+    //creo la riga
     const row = document.createElement('div');
     row.className = 'riga_mercato';
+
+    //creo il campo foto nella riga
     const campoFoto = document.createElement('div');
     campoFoto.className = 'campo_foto';
     const img = document.createElement('img');
     img.src = calciatore.url_foto;
     campoFoto.appendChild(img);
+
+    //creo il campo info nella riga
+
     const campoInfo = document.createElement('div');
     campoInfo.className = 'campo_info';
     const nomeCognome = document.createElement('h2');
-    nomeCognome.textContent = `${calciatore.nome} ${calciatore.cognome}`;
-    const ul_info = document.createElement('ul');
-    console.log("tipo:",typeof calciatore);
+    nomeCognome.style.marginBlockEnd = '0.20em';
+
+    const informazioni = document.createElement('div');
+    informazioni.className = 'informazioni';
+
+    if(calciatore.nome == null) nomeCognome.textContent = `${calciatore.cognome}`;
+    else nomeCognome.textContent = `${calciatore.nome} ${calciatore.cognome}`;
+
+    //divisione in due liste
+
+    const lista1 = document.createElement('ul');
+    lista1.style.marginBlockStart = '0.20em';
+    
+    const lista2 = document.createElement('ul');
+    lista2.style.marginBlockStart = '0.20em';
 
     //Itero sui campi giocatore per costruire la lista
-    Object.keys(calciatore).forEach(key => {
-        if(key != 'url_foto' && key != 'id_player' && key != 'id' && key != 'nome' && key != 'cognome'){
-            const info = document.createElement('li');
-            if (key === 'data_nascita') {
-                info.textContent = `${KeyConverter(key)}: ${convertDate(calciatore[key])}`;
-            } else if (key === 'nazionalita') {
-                info.textContent = `${KeyConverter(key)}: ${calciatore[key].toUpperCase()}`;
-            } else if (key === 'ruolo') {
-                info.textContent = `${KeyConverter(key)}: ${calciatore[key].toUpperCase()}`;
-            } else {
-                info.textContent = `${KeyConverter(key)}: ${calciatore[key]}`;
-            }
-            info.textContent = `${KeyConverter(key)}: ${calciatore[key]}`;
-            ul_info.appendChild(info);
+
+    keys = Object.keys(calciatore);
+    keys = keys.filter(key => !['id', 'nome', 'cognome', 'url_foto', 'id_player'].includes(key));
+
+    for(const key of keys){
+        if(key == 'presenze') break;
+        const li = document.createElement('li');
+        li.textContent = `${KeyConverter(key)}: ${calciatore[key]}`;
+        if(key == 'data_nascita'){
+            li.textContent = `${KeyConverter(key)}: ${convertDate(calciatore[key])}`;
         }
-    })
+        lista1.appendChild(li);
+    }
+
+    let start = false;
+    for(const key of keys){
+        if(key == 'presenze') start = true;
+        if(start){
+            const li = document.createElement('li');
+            li.textContent = `${KeyConverter(key)}: ${calciatore[key]}`;
+            lista2.appendChild(li);
+        }
+    }
+
+    informazioni.appendChild(lista1);
+    informazioni.appendChild(lista2);
     campoInfo.appendChild(nomeCognome);
-    campoInfo.appendChild(ul_info);
+    campoInfo.appendChild(informazioni);
     row.appendChild(campoFoto);
     row.appendChild(campoInfo);
+
+    //creo il bottone compra
+
+    const bottone = document.createElement('button');
+    const spanbottone = document.createElement('span');
+    spanbottone.textContent = 'Acquista';
+    spanbottone.className = 'button_top';
+    bottone.appendChild(spanbottone);
+    row.appendChild(bottone);
     document.getElementById('finestramercato').appendChild(row);
 }
 
