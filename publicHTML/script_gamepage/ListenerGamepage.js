0190ba94dad0bccb7da301143a62560355bf68e2
markoldmark <marcoldmark@gmail.com>
2025-05-24 17:31:21 +0200

    corretto trattino basso che gianuca non sa scrivere

--- publicHTML/script_gamepage/ListenerGamepage.js
+++ publicHTML/script_gamepage/ListenerGamepage.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //ELEMENTI DA NASCONDERE ALL'INIZIO (sezioni, condizione,switch,timer,pedine)
     document.querySelector('.sezione-sx').classList.add('hidden');
-    document.querySelector('.sezione_dx').classList.add('hidden');
+    document.querySelector('.sezione-dx').classList.add('hidden');
     document.querySelector('.switch').classList.add('hidden');
     document.querySelector('.background-overlay').classList.add('hidden');
     document.querySelector('.condition-container').classList.add('hidden');
