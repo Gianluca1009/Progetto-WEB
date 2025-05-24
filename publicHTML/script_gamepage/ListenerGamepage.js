8b24379ec7f1d9bfa4b1791f0c2b77a29f5f5089
Gianluca2003 <gianlu.nanni@gmail.com>
2025-05-24 16:52:15 +0200

    riga_draft => riga-draft

--- publicHTML/script_gamepage/ListenerGamepage.js
+++ publicHTML/script_gamepage/ListenerGamepage.js
@@ -64,7 +64,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     // PERMETTE DI NON FAR VEDERE IL MENU DI CONTESTO PER IL GAME CONTAINER COL TASTO DESTRO 
-    document.querySelectorAll('.riga_draft').forEach(container => {
+    document.querySelectorAll('.riga-draft').forEach(container => {
         container.addEventListener('contextmenu', (event) => {
             event.preventDefault();
         });
